3bf8d64a2c4ccf6a08b55431c84ea90029aa9613
Tushar Bhite <tushbhite@gmail.com>
2022-04-15 14:44:05 +0530

    Portfolio V1.0

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-This is MAIN branch
 # AngularAppHeroku
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.4.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { AppRoutingModule } from './app-routing.module';
-import { CarouselComponent } from './carousel/carousel.component';
-import { ToastComponent } from './toast/toast.component';
-import { WarningComponent } from './warning/warning.component';
-import { CardGroupComponent } from './card-group/card-group.component';
-import { FooterComponent } from './footer/footer.component';
-import { LinkListComponent } from './link-list/link-list.component';
-import { TopTemComponent } from './top-tem/top-tem.component';
 
-const routes: Routes = [
-  { path: 'home', component: NavigationComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-]
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavigationComponent,
-    CarouselComponent,
-    ToastComponent,
-    WarningComponent,
-    CardGroupComponent,
-    FooterComponent,
-    LinkListComponent,
-    TopTemComponent,
-    
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot(routes)
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
