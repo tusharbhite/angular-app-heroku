3f8422e68d832ba2e1c62adf039af818ce9c088e
Tushar Bhite <tushbhite@gmail.com>
2022-04-16 00:15:39 +0530

    Portfolio V2.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TypingComponent } from './typing/typing.component';
+import { LoaderComponent } from './loader/loader.component';
+import { ThanksComponent } from './thanks/thanks.component';
+import { AccordionComponent } from './accordion/accordion.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TypingComponent,
+    LoaderComponent,
+    ThanksComponent,
+    AccordionComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule
